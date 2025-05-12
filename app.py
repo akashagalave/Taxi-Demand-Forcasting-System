b07219a8ca37f6afd601d3394522ed814e27a463
akashagalave <akashagalaveaaa1@gmail.com>
2025-05-12 23:38:12 +0530

    Building and pushing docker image to aws ECR

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ mlflow.set_tracking_uri("https://dagshub.com/akashagalaveaaa1/Taxi-Demand-Predic
 
 # get model name
 registered_model_name = 'uber_demand_prediction_model'
-stage = "Staging"
+stage = "Production"
 model_path = f"models:/{registered_model_name}/{stage}"
 
 # load the latest model from model registry
